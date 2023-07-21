8286aad3f14e4584621142da6f3a70bf4f06fe41
Ashok Kumar <28ash92@gmail.com>
2023-07-21 21:26:19 +0530

    Create mini_calculator.py
    mini calculator

---
+++ mini_calculator.py
@@ -0,0 +1,13 @@
+a=int(input("A:"))
+b=int(input("B:"))
+opration=input("add/sub/mul/div:")
+if(opration=="add"):
+    print(a+b)
+elif(opration=="sub"):
+    primt(a-b)
+elif(opration=="mul"):
+    print(a*b)
+elif(opration=="div"):
+    print(a/b)
+else:
+    print("invalid opration")
